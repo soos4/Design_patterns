f282a383de95e9ec4dccab40d10a831861355d61
Robert Soos <robert.soos@zeiss.com>
2023-11-12 22:11:49 +0100

    Added project and client to Flyweight

---
+++ Flyweight/Flyweight/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CharacterFactory factory = new CharacterFactory();
+
+            List<Character> characters = new List<Character>
+            {
+                factory.GetCharacter('A'),
+                factory.GetCharacter('B'),
+                factory.GetCharacter('B'),
+                factory.GetCharacter('Z'),
+                factory.GetCharacter('A'),
+                factory.GetCharacter('B')
+            };
+
+            foreach (Character character in characters)
+            {
+                character.Display(15);
+            }
+        }
+    }
+}

4ac693cfb36d61d5b27c339655de54b479a5a10a
Robert Soos <robert.soos@zeiss.com>
2023-11-12 22:03:18 +0100

    Added client for Facade and solutions

---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Customer customer1 = new Customer("John");
+            Mortgage mortgage = new Mortgage();
+
+            Console.WriteLine("Checking if customer is eligible for 5000:");
+            bool isEligible = mortgage.IsEligible(customer1, 5000);
+            Console.WriteLine(isEligible);
+
+            Console.WriteLine("Checking if customer is eligible for 20000:");
+            isEligible = mortgage.IsEligible(customer1, 20000);
+            Console.WriteLine(isEligible);
+        }
+    }
+}

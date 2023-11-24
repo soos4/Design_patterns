c160ba202c88ae31f0fc2b361c008fe3eee5921d
Robert Soos <robert.soos@zeiss.com>
2023-11-24 15:46:41 +0100

    Added project and program to Proxy

---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Proxy
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Math math = new MathProxy();
+
+            Console.WriteLine(math.Add(15, 24));
+            Console.WriteLine(math.Div(10, 2));
+            Console.WriteLine(math.Mul(30, 4));
+            Console.WriteLine(math.Sub(15, 12));
+            Console.WriteLine(math.Mul(32, 2));
+            Console.WriteLine(math.Add(10, 13));
+            Console.WriteLine(math.Add(43, 51));
+        }
+    }
+}
